6c26fcb46aeb5d2d8e540942e68d27c3bbcd8b22
Joshua Moravec <joshua.moravec@gmail.com>
2016-01-03 11:59:11 -0600

    Add some more static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | DOJG Index"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | DOJG Index"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | DOJG Index"
+  end
 end
